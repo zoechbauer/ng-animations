21c82f9a6a64f3a0ba0b43d0361eb10e5b7c0817
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-28 18:50:29 +0200

    Animate with advanced Transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,16 +27,47 @@ import {
         })
       ),
       transition('normal <=> highlighted', animate(300)),
-      // transition('highlighted => normal', animate(800)),
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          backgroundColor: 'green',
+          transform: 'translateX(0) scale(0.5)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500)),
     ]),
   ],
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
